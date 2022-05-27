e74f19df88cca8babbd10bb291f10d6c2e0b5e6d
Andres <x3enos@gmail.com>
2022-05-27 06:17:23 +0200

    Added rvmrc and json file for documents

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::DocumentsController < Api::V1::BaseController
 
   def index
-    documents = current_user.documents
+    @documents = current_user.documents.order(created_at: :desc)
     render 'index', formats: :json
   end
 
   def create
     @document = current_user.documents.new(document_params)
     if @document.save
-      render partial: 'api/v1/documents/show', formats: :json,  locals: { document: @document }
+      render partial: 'api/v1/documents/document', formats: :json, locals: { document: @document }
     else
       render json: { errors: @document.errors }, status: 400
     end
--- app/views/api/v1/documents/_document.json.jbuilder
+++ app/views/api/v1/documents/_document.json.jbuilder
@@ -1,1 +1,5 @@
 json.(document, :id, :name)
+json.images document.images do |image|
+  json.url image.url
+  json.name image.filename.to_s
+end
