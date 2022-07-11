f18df6e7534cd1082ed7567488605e9b2056bbde
Andres <x3enos@gmail.com>
2022-07-11 10:39:21 +0200

    Filtered documents by name, box and date

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::DocumentsController < Api::V1::BaseController
 
   def index
-    @documents = current_user.documents.order(created_at: :desc)
+    @documents = current_user.documents.search(params).order(created_at: :desc)
     render 'index', formats: :json
   end
 
--- app/models/document.rb
+++ app/models/document.rb
@@ -2,4 +2,29 @@ class Document < ApplicationRecord
   has_many_attached :images
   belongs_to :user
   belongs_to :box, optional: true
+
+  def self.search(params)
+    select_by_name(params[:name]).
+      select_by_box(params[:box_id]).
+      select_by_date(params[:date])
+  end
+
+  private
+
+  def self.select_by_name(value)
+    value.present? ? where('name LIKE ?', "%#{value}%") : all
+  end
+
+  def self.select_by_box(value)
+    value.present? ? where(box_id: value) : all
+  end
+
+  def self.select_by_date(value)
+    if value.present?
+      date = Date.parse(value)
+      where("created_at BETWEEN ? and ?", date.beginning_of_day, date.end_of_day)
+    else 
+      all
+    end
+  end
 end
