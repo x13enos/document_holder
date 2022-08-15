7b0c66659a72da6baf5fa1bccef7fe5ea90ba499
Andres <x3enos@gmail.com>
2022-08-15 10:02:45 +0200

    Adjust app for k8s

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,4 +8,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
+  allow do
+    origins "document-holder.andres-sild.com"
+    resource "*",
+             headers: :any,
+             credentials: true,
+             methods: %i[get post options put patch delete]
+  end
 end
